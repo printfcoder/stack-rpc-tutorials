a116f915f6934bcbfc8ccf929414d26dd8cb747d
Shu Xian <i@shux.me>
2019-04-20 01:13:59 +0800

    优化接口回参

--- microservice-in-micro/part2/user-srv/handler/service.go
+++ microservice-in-micro/part2/user-srv/handler/service.go
@@ -30,6 +30,7 @@ func (e *Service) QueryUserByName(ctx context.Context, req *s.Request, rsp *s.Re
 
 	user, err := userService.QueryUserByName(req.UserName)
 	if err != nil {
+		rsp.Success = false
 		rsp.Error = &s.Error{
 			Code:   500,
 			Detail: err.Error(),
@@ -39,6 +40,7 @@ func (e *Service) QueryUserByName(ctx context.Context, req *s.Request, rsp *s.Re
 	}
 
 	rsp.User = user
+	rsp.Success = true
 
 	return nil
 }
