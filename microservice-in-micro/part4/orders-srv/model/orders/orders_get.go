da2c1997920335f16f71ab896cd549c32d2d07bf
Shu Xian <i@shux.me>
2019-04-29 22:52:12 +0800

    orders-srv 配置中心

---
+++ microservice-in-micro/part4/orders-srv/model/orders/orders_get.go
@@ -0,0 +1,25 @@
+package orders
+
+import (
+	proto "github.com/micro-in-cn/tutorials/microservice-in-micro/part4/orders-srv/proto/orders"
+	"github.com/micro-in-cn/tutorials/microservice-in-micro/part4/plugins/db"
+	"github.com/micro/go-log"
+)
+
+// GetOrder 获取订单
+func (s *service) GetOrder(orderId int64) (order *proto.Order, err error) {
+
+	order = &proto.Order{}
+
+	// 获取数据库
+	o := db.GetDB()
+	// 查询
+	err = o.QueryRow("SELECT id, user_id, book_id, inv_his_id, state FROM orders WHERE id = ?", orderId).Scan(
+		&order.Id, &order.UserId, &order.BookId, &order.InvHistoryId, &order.State)
+	if err != nil {
+		log.Logf("[GetOrder] 查询数据失败，err：%s", err)
+		return
+	}
+
+	return
+}
