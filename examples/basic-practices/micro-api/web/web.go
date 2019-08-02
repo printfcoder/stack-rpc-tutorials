73cdc383908234609541b6e1677c23a3a121400d
shu xian <i@shux.me>
2019-08-02 18:42:14 +0800

    增加web路由

--- examples/basic-practices/micro-api/web/web.go
+++ examples/basic-practices/micro-api/web/web.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/micro/go-micro/web"
@@ -28,8 +30,31 @@ func main() {
 	// websocket interface
 	service.HandleFunc("/websocket/hi", hi)
 
+	// websocket interface
+	service.HandleFunc("/websocket/hi2", hi2)
+
+	// websocket interface
+	service.HandleFunc("/websocket/hi3/hi3", hi2)
+
 	if err := service.Run(); err != nil {
 		log.Fatal("Run: ", err)
+	}
+}
+
+func hi2(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+
+	_ = r.ParseForm()
+	// 返回结果
+	response := map[string]interface{}{
+		"ref":  time.Now().UnixNano(),
+		"data": "Hello! " + r.Form.Get("name"),
+	}
+
+	// 返回JSON结构
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
 	}
 }
 
