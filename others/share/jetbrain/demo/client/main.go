1e00d4dcc68673c697d24bba41ece250d976d0a9
shu xian <i@shux.me>
2020-04-30 00:02:19 +0800

    JETBRAIN

--- others/share/jetbrain/demo/client/main.go
+++ others/share/jetbrain/demo/client/main.go
@@ -1,1 +1,24 @@
-package client
+package main
+
+import (
+	"context"
+	"fmt"
+
+	goland "github.com/micro-in-cn/tutorials/others/share/jetbrain/demo/proto"
+	"github.com/micro/go-micro/v2"
+)
+
+func main() {
+	service := micro.NewService(micro.Name("goland.client"))
+	service.Init()
+
+	greeter := goland.NewGreeterService("goland.greeter", service.Client())
+
+	rsp, err := greeter.Hello(context.TODO(), &goland.HelloRequest{Name: "Goland 中国"})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(rsp.Greeting)
+}
--- others/share/jetbrain/demo/server/main.go
+++ others/share/jetbrain/demo/server/main.go
@@ -1,1 +1,27 @@
-package server
+package main
+
+import (
+	"context"
+
+	goland "github.com/micro-in-cn/tutorials/others/share/jetbrain/demo/proto"
+	"github.com/micro/go-micro/v2"
+)
+
+type Greeter struct{}
+
+func (g *Greeter) Hello(ctx context.Context, req *goland.HelloRequest, rsp *goland.HelloResponse) error {
+	rsp.Greeting = "Hello，" + req.Name
+	return nil
+}
+
+func main() {
+	service := micro.NewService(
+		micro.Name("goland.greeter"),
+	)
+	service.Init()
+	goland.RegisterGreeterHandler(service.Server(), new(Greeter))
+
+	if err := service.Run(); err != nil {
+		panic(err)
+	}
+}
